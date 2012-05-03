14a04fd17b8e2c52a08c21c4f2ac29de60472e6f
syota <syota@inoue-shouta-no-MacBook-Pro.local>
2012-05-03 16:03:24 +0900

    proから移行

---
+++ otonari.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>おとなり動画</title>
+<style type="text/css">
+#gbx4{background-color:#2d2d2d;background-image:none;_background-image:none;background-position:0 -138px;background-repeat:repeat-x;border-bottom:10px solid #000;font-size:24px;height:29px;_height:30px;opacity:1;filter:alpha(opacity=100);position:absolute;top:100;width:100%;left:0%;z-index:990}
+</style>
+</head>
+<body>
+<div style="position:absolute;top:10px;">
+<img src="otonari_logo.png">
+</div>
+<div id="gbx4"></div>
+<div style="position:absolute;top:300px;">
+<script type="text/javascript" src="http://ext.nicovideo.jp/thumb_watch/sm15453030?&n=1"></script><noscript><a href="http://www.nicovideo.jp/watch/sm15453030" target="_blank">【ニコニコ動画】スーパーストリートファイターIV AEを華やかに実況せんとす　第41話</a></noscript>
+<script type="text/javascript" src="http://ext.nicovideo.jp/thumb_watch/sm15453030?&n=1"></script><noscript><a href="http://www.nicovideo.jp/watch/sm15453030" target="_blank">【ニコニコ動画】スーパーストリートファイターIV AEを華やかに実況せんとす　第41話</a></noscript>
+<script type="text/javascript" src="test.js"></script>
+</div>
+</body>
---
+++ yusya_rslt.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>おお勇者よ</title>
+  </head>
+  <body>
+  <div align="center">
+  <img src="./ousama.JPG">
+	<br> 
+	<img src="./msg.php?value=<?php echo $_GET['value'] ?>">
+  </div>
+  </body>
+</html>
