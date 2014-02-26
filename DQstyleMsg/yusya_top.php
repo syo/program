b0c2224c1b003416b23fb28d410806a0042bcf0a
shota inoue <wisdom1027@gmail.com>
2014-02-26 15:23:16 +0900

    暫定状況に更新

---
+++ DQstyleMsg/yusya_top.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>おお勇者よ</title>
+<link rel="stylesheet" type="text/css" href="site.css">
+</head>
+<body>
+
+<div class="title">
+	おお勇者よ
+</div>
+<div align=center>
+<form action="./yusya_rslt.php" method="GET">
+<input type="text" name="value" class="txtarea" value="" >
+<input type="submit">
+</form>
+</div>
+</body>
+</html>
+
